9df179954289852e7766bcd885d73d688322cf07
José Stélio Malcher Jr <contato@josemalcher.net>
2020-05-21 22:14:01 -0300

    3.8. Construtores & Destrutores

--- 03-Conceitos_Basicos_de_OO/car.php
+++ 03-Conceitos_Basicos_de_OO/car.php
@@ -7,6 +7,20 @@ class car
     public $years;
     public $model;
 
+    /**
+     * car constructor.
+     * @param $color
+     * @param $years
+     * @param $model
+     */
+    public function __construct($color, $years, $model)
+    {
+        $this->color = $color;
+        $this->years = $years;
+        $this->model = $model;
+    }
+
+
     public function run()
     {
         return $this->model . " Car is Running";
@@ -16,17 +30,24 @@ class car
     {
         return $this->model . " Car is Stop";
     }
+
+    public function __destruct()
+    {
+        print "\n REMOVENDO Objeto" . __CLASS__ ;
+    }
+
+
 }
 
-$car = new Car();
-$car->model = "Car";
-$car->color = "red";
-$car->years = 2000;
+$car = new Car("Red", "2000", "HONDA");
+//$car->model = "Car";
+//$car->color = "red";
+//$car->years = 2000;
 
-$car2 = new Car();
-$car2->model = "Car 2";
-$car2->color = "blue";
-$car2->years = 2020;
+$car2 = new Car("BLUE0", "2020", "RENAULT");
+//$car2->model = "Car 2";
+//$car2->color = "blue";
+//$car2->years = 2020;
 
 print $car->run();
 print "\n";
@@ -37,3 +58,11 @@ print "\n";
 print $car2->run();
 print "\n";
 print $car2->stop();
+/*
+HONDA Car is Running
+HONDA Car is Stop
+RENAULT Car is Running
+RENAULT Car is Stop
+ REMOVENDO Objetocar
+ REMOVENDO Objetocar
+*/
--- README.md
+++ README.md
@@ -92,6 +92,68 @@ print $car2->stop();
 
 #### 3.8. Construtores & Destrutores
 
+```php
+class car
+{
+    public $color;
+    public $years;
+    public $model;
+
+    /**
+     * car constructor.
+     * @param $color
+     * @param $years
+     * @param $model
+     */
+    public function __construct($color, $years, $model)
+    {
+        $this->color = $color;
+        $this->years = $years;
+        $this->model = $model;
+    }
+    public function run()
+    {
+        return $this->model . " Car is Running";
+    }
+    public function stop()
+    {
+        return $this->model . " Car is Stop";
+    }
+    public function __destruct()
+    {
+        print "\n REMOVENDO Objeto" . __CLASS__ ;
+    }
+}
+$car = new Car("Red", "2000", "HONDA");
+//$car->model = "Car";
+//$car->color = "red";
+//$car->years = 2000;
+
+$car2 = new Car("BLUE0", "2020", "RENAULT");
+//$car2->model = "Car 2";
+//$car2->color = "blue";
+//$car2->years = 2020;
+
+print $car->run();
+print "\n";
+print $car->stop();
+
+print "\n";
+
+print $car2->run();
+print "\n";
+print $car2->stop();
+/*
+HONDA Car is Running
+HONDA Car is Stop
+RENAULT Car is Running
+RENAULT Car is Stop
+ REMOVENDO Objetocar
+ REMOVENDO Objetocar
+*/
+```
+
+
 #### 3.9. Herança
 
 #### 3.10. Visibilidade
