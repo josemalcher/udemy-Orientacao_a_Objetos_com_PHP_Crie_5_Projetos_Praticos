e36abbb5d5b243f220ccc1a0a66b6ace4fd61d10
José Stélio Malcher Jr <contato@josemalcher.net>
2020-06-07 23:51:50 -0300

    9.49. Produtos na View

--- 09-Projeto-2-Catalogo_de_Produtos/src/Controller/HomeController.php
+++ 09-Projeto-2-Catalogo_de_Produtos/src/Controller/HomeController.php
@@ -11,14 +11,14 @@ class HomeController
     {
         $pdo = new \PDO('mysql:dbname=formacao_php;host=localhost', 'root', '');
 
-        $products = new Product($pdo);
-        var_dump($products->findAll());
-        die();
+
+//        var_dump($products->findAll());
+//        die();
 
         $view = new View('site/index.phtml');
         //$view->nome = "José Malcher Jr";
 
-        $view->products = $product->findAll();
+        $view->products = ( new Product($pdo))->findAll();
         return $view->render();
     }
 }
